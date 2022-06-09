84ba71a6f27429cfa87715436d413e4e2799c304
JoraNoe <57209261+JoraNoe@users.noreply.github.com>
2022-06-09 11:22:48 +0800

    Revert "."
    This reverts commit 794dd7a7a8393efc15b34ef29ef2f3175874b6fa.

--- JoreNoe/Queue/RBMQ/QueueManager.cs
+++ JoreNoe/Queue/RBMQ/QueueManager.cs
@@ -1,17 +1,17 @@
 ﻿using Newtonsoft.Json;
-//using RabbitMQ.Client;
-//using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 namespace JoreNoe.Queue.RBMQ
 {
     public class QueueManager
     {
-        //private static ConnectionFactory ConectionFactory;
+        private static ConnectionFactory ConectionFactory;
         private static string QueueName = Register._ChannelName;
         public QueueManager()
         {
-           // ConectionFactory = Register.ConectionFactory;
+            ConectionFactory = Register.ConectionFactory;
         }
 
 
@@ -21,53 +21,53 @@ namespace JoreNoe.Queue.RBMQ
         /// <typeparam name="T"></typeparam>
         /// <param name="Entity"></param>
         /// <param name="QueueChannle"></param>
-        //public static void SendPublish<T>(T Entity, string Type = ExchangeType.Topic)
-        //{
-        //    using (var connection = ConectionFactory.CreateConnection())
-        //    {
-        //        using (var channel = connection.CreateModel())
-        //        {
-        //            channel.ExchangeDeclare(QueueName, Type);
-        //            channel.QueueDeclare(QueueName, false, false, false, null);
-        //            string message = JsonConvert.SerializeObject(Entity);
-        //            var properties = channel.CreateBasicProperties();
-        //            properties.DeliveryMode = 2;
-        //            var body = Encoding.UTF8.GetBytes(message);
-        //            channel.BasicPublish("", QueueName, properties, body);
-        //        }
-        //    }
-        //}
+        public static void SendPublish<T>(T Entity, string Type = ExchangeType.Topic)
+        {
+            using (var connection = ConectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(QueueName, Type);
+                    channel.QueueDeclare(QueueName, false, false, false, null);
+                    string message = JsonConvert.SerializeObject(Entity);
+                    var properties = channel.CreateBasicProperties();
+                    properties.DeliveryMode = 2;
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", QueueName, properties, body);
+                }
+            }
+        }
 
 
-        ///// <summary>
-        ///// 接收消息
-        ///// </summary>
-        ///// <typeparam name="T"></typeparam>
-        ///// <param name="Custome"></param>
-        //public static void Receive<T>(ICustome Custome) where T : class
-        //{
-        //    using (var connection = ConectionFactory.CreateConnection())
-        //    {
-        //        using (var channel = connection.CreateModel())
-        //        {
-        //            var consumer = new EventingBasicConsumer(channel);
-        //            channel.BasicConsume(QueueName, true, consumer);
-        //            consumer.Received += (model, ea) =>
-        //            {
-        //                var body = ea.Body;
-        //                var message = Encoding.UTF8.GetString(body);
+        /// <summary>
+        /// 接收消息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Custome"></param>
+        public static void Receive<T>(ICustome Custome) where T : class
+        {
+            using (var connection = ConectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume(QueueName, true, consumer);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
 
-        //                Custome.ConSume(new CustomeContent<T>
-        //                {
-        //                    QueueName = QueueName,
-        //                    Context = (T)JsonConvert.DeserializeObject(message)
-        //                });
+                        Custome.ConSume(new CustomeContent<T>
+                        {
+                            QueueName = QueueName,
+                            Context = (T)JsonConvert.DeserializeObject(message)
+                        });
 
-        //            };
+                    };
 
-        //        }
-        //    }
+                }
+            }
 
-        //}
+        }
     }
 }
